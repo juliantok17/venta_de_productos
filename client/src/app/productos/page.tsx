cb757bb955c999ace68569699d16627148003ea2
juliantok17 <juliantok17@gmail.com>
2025-02-11 17:32:22 +0100

    Limpieza de codigo inicial

---
+++ client/src/app/productos/page.tsx
@@ -0,0 +1,8 @@
+export default function Productos() {
+    return (
+        <main>
+            <h1>Productos</h1>
+            <p>Aquí mostraremos los productos</p>
+        </main>        
+    );
+}
