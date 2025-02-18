53e4f99b03f66aad01aa36496e1fd843299dc29e
juliantok17 <29335583+juliantok17@users.noreply.github.com>
2025-02-18 21:10:00 +0100

    cambio parrafo en page.tsx productos -prueba-

--- client/src/app/productos/page.tsx
+++ client/src/app/productos/page.tsx
@@ -2,7 +2,7 @@ export default function Productos() {
     return (
         <main>
             <h1>Productos</h1>
-            <p>Aquí mostraremos los productos</p>
+            <p>Aquí mostraremos la lista productos</p>
         </main>        
     );
 }
